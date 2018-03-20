6bd270b08990498cc7a81b041413e716d77e3a49
Sam Niechcial <samniechcial@gmail.com>
2018-03-20 15:14:12 +0000

    Bike added & empty tests built

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,13 +1,19 @@
-require 'DockingStation.rb'
+require 'docking_station'
 
-describe "DockingStation" do
-  it "takes a request from a person to release a bike" do
-    expect(DockingStation.accept_request).to eq true
-  end
-  it "checks the bike is working" do
-    expect(DockingStation.bike_check).to eq true or false
-  end
-  it "releases the bike if it is working" do
-    expect(DockingStation.release_bike).to eq true
+# describe "DockingStation" do
+#  it "takes a request from a person to release a bike" do
+#    expect(DockingStation.accept_request).to eq true
+#  end
+#  it "checks the bike is working" do
+#    expect(DockingStation.bike_check).to eq true or false
+#  end
+#   it "releases the bike if it is working" do
+#     expect(DockingStation.release_bike).to eq true
+#   end
+# end
+
+describe DockingStation do
+  it 'responds to release_bike' do
+    expect(subject).to respond_to :release_bike
   end
 end
